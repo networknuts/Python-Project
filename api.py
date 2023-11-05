465667b8d36ab474619aa6f4aec18c524d097975
aryan <aryan@networknuts.net>
2023-11-05 01:55:36 -0500

    fixed typo error in session

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import requests
 url = 'https://httpbin.org/uuid'
 
 def fetch(session,url):
-    with sessiion.get(url) as response:
+    with session.get(url) as response:
         print(response.json()['uuid'])
         with open('/tmp/tokencontent.txt','a') as tokenfile:
             tokenfile.write(response.json()['uuid']+'\n')
